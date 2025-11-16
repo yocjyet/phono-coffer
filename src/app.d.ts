1dff20b4ee2b4c52d5781dfc6cbdd1b17f3f115a
yocjyet <git@yocjyet.dev>
2025-11-16 14:31:08 +0900

    feat: integrate unplugin-icons for enhanced icon support in the application

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="unplugin-icons/types/svelte" />
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		}),
 		devtoolsJson(),
 		paraglideVitePlugin({
 			project: './project.inlang',
